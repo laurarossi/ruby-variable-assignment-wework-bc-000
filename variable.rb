b24415db43cae13dc4ced74f51058ea9608dfbaf
Laura Rossi <laura.rossi@wework.com>
2018-02-12 04:20:19 +0000

    automatically backed up by learn

--- variable.rb
+++ variable.rb
@@ -1,5 +1,5 @@
  # Define a local variable called 'greeting',
  # give it the value of the string "Hello World"
  
-current_greeting = "Hello World"
-puts "#{current_greeting}."
+greeting = "Hello World"
+puts "#{greeting}."
